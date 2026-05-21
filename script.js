5c11d935833f403150c83306b0b9228f9964e449
iran <iran.csousa21@gmail.com>
2026-05-21 17:59:27 -0300

    ultimos acabamentos

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
+// async permite usar await dentro da função
+// await faz o código esperar a resposta da API chegar
+
 async function buscarCEP(){
-    console.log("Acessou a função de busca")
+
     let cep = document.getElementById("cep").value
+
+    // Faz requisição para a API ViaCEP usando o CEP digitado
     let resposta = await fetch(
         `https://viacep.com.br/ws/${cep}/json/`
     )
+
+    // Converte a resposta da API para JSON
     let dados = await resposta.json()
 
+    // Exibe os dados retornados pela API na tela
     document.getElementById("resultado").innerHTML =
     `
     <p>Rua: ${dados.logradouro}</p>
@@ -13,5 +21,4 @@ async function buscarCEP(){
     <p>Cidade: ${dados.localidade}</p>
     <p>Estado: ${dados.uf}</p>
     `
-    console.log(dados)
 }
